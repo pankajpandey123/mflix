df29df5ad41d2675446fec554edce156ab91783e
Pankaj Pandey <60574346+pankajpandey123@users.noreply.github.com>
2021-11-19 14:12:40 +0100

    update db name - update

--- src/test/java/mflix/lessons/MongoClientLesson.java
+++ src/test/java/mflix/lessons/MongoClientLesson.java
@@ -29,7 +29,7 @@ public class MongoClientLesson extends AbstractLesson {
 
   private MongoCollection<Document> collection;
 
-  private String uri = "<YOUR SRV STRING from the application.properties file>";
+  private String uri = "mongodb+srv://m220student:m220student@mflix.lv23k.mongodb.net/sample_mflix";
 
   private Document document;
 
@@ -121,7 +121,7 @@ public class MongoClientLesson extends AbstractLesson {
     you need to go over the contents more than once.
      */
 
-    Assert.assertTrue(dbnames.contains("mflix"));
+    Assert.assertTrue(dbnames.contains("sample_mflix"));
 
     /*
     Then we have our MongoDatabase object. We will use this object to
@@ -129,7 +129,7 @@ public class MongoClientLesson extends AbstractLesson {
     database level read preferences, read concerns and write concerns.
      */
 
-    database = mongoClient.getDatabase("mflix");
+    database = mongoClient.getDatabase("sample_mflix");
 
     ReadPreference readPreference = database.getReadPreference();
 
@@ -154,7 +154,7 @@ public class MongoClientLesson extends AbstractLesson {
      */
 
     mongoClient = MongoClients.create(uri);
-    database = mongoClient.getDatabase("mflix");
+    database = mongoClient.getDatabase("sample_mflix");
     collection = database.getCollection("movies");
 
     /*
